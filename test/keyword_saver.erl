d807f58f263ba0d8da405e763f6eb0f3e81f8547
Yuki Nitta <yuki@nit2a.com>
2011-07-30 01:44:34 +0900

    Modified: test application

--- test/keyword_saver.erl
+++ test/keyword_saver.erl
@@ -1,24 +1,40 @@
--module(start).
--vsn(1.0).
+-module(keyword_saver).
+-vsn(1.01).
 -author('Yuki Nitta <yuki@nit2a.com>').
 
--export([start/0]).
+-export([start_save/0]).
+-export([init/1]).
+
+-behavior(supervisor).
+
+-define(SEARCH_KEYWORDS, unilib:utf8binary_to_list(unilib:utf8_to_binary([
+	"UTF8_keyword01",
+	"UTF8_keyword02",
+	"UTF8_keyword03",
+	"UTF8_keyword04",
+	"UTF8_keyword05"
+]))).
 
 
-start() ->
+start_save() ->
 	application:start(twitter),
-	spawn(stream_server, request_stream, [
-		couchdb,
-		[
-			"keyword01",
-			"keyword02",
-			"keyword03",
-			"keyword04",
-			"keyword05",
-			"keyword06",
-			"keyword07",
-			"keyword08",
-			"keyword09",
-			"keyword10"
-		]
-	]).
+	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+
+
+%%%
+%% Callback Function on OTP/supervisor
+%
+
+init(_Args) ->
+	UsrChildren = [
+		{
+			request_stream,
+			{stream_server, request_stream, [couchdb, ?SEARCH_KEYWORDS]},
+			temporary,
+			1000,
+			worker,
+			dynamic
+		}
+	],
+	{ok, {{one_for_one, 2, 10}, UsrChildren}}.
